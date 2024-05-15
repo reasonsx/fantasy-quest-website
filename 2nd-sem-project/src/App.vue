d2ed849b606186584e5bb13f2379a0e39906953d
reasonsx <kamkre01@easv365.dk>
2024-05-15 11:53:19 +0200

    variables

--- 2nd-sem-project/src/App.vue
+++ 2nd-sem-project/src/App.vue
@@ -13,15 +13,15 @@ import { RouterLink, RouterView } from 'vue-router'
       <p>change to main test</p>
     </div>
   </header>
-  <div class="svg-test">
-    <svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.dev/svgjs" viewBox="0 0 700 700" width="700" height="700" opacity="1"><defs><filter id="nnnoise-filter" x="-20%" y="-20%" width="140%" height="140%" filterUnits="objectBoundingBox" primitiveUnits="userSpaceOnUse" color-interpolation-filters="linearRGB">
-	<feTurbulence type="fractalNoise" baseFrequency="0.154" numOctaves="4" seed="15" stitchTiles="stitch" x="0%" y="0%" width="100%" height="100%" result="turbulence"></feTurbulence>
-	<feSpecularLighting surfaceScale="4" specularConstant="0.75" specularExponent="20" lighting-color="#f3d5b9" x="0%" y="0%" width="100%" height="100%" in="turbulence" result="specularLighting">
-    		<feDistantLight azimuth="3" elevation="100"></feDistantLight>
-  	</feSpecularLighting>
-  
-</filter></defs><rect width="700" height="700" fill="#fff6e9"></rect><rect width="700" height="700" fill="#f3d5b9" filter="url(#nnnoise-filter)"></rect></svg>
-  </div>
+  <body>
+    
+  </body>
 
   <RouterView />
 </template>
+<style>
+body { background-image: url('../src/assets/img/background-svg.svg'); }
+p {
+  font-family: 'Dark Grotesque', sans-serif;
+}
+</style>
