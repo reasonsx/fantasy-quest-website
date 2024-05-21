7ce70765bd1fecf2ce06ff35d971393ccf770002
reasonsx <kamkre01@easv365.dk>
2024-05-21 13:00:19 +0200

    Update DayView.vue

--- 2nd-sem-project/src/views/DayView.vue
+++ 2nd-sem-project/src/views/DayView.vue
@@ -579,7 +579,100 @@ iframe{
 .sixth-container .btn{
     width: 350px;
 }
+footer {
+    width: 100%;
+    height: auto;
+    background-color: var(--red-color);
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
 
+}
+
+.bottom-footer {
+    width: 1120px;
+    /* height: 64px; */
+    display: flex;
+    justify-content: center;
+    gap: 16px;
+    align-items: center;
+    /* flex-direction: column; */
+    margin: 0 0 24px 0;
+
+    @media screen and (max-width: 768px) {
+        width: 360px
+    }
+
+}
+footer p {
+    color: var(--light-papirus);
+    font-family: var(--secondary-font);
+    font-size: 24px;
+    font-weight: 400;
+
+}
+
+.footer-container {
+    width: 1120px;
+    height: auto;
+    box-sizing: border-box;
+    overflow: hidden;
+    display: flex;
+    justify-content: space-between;
+    padding: 0 0 24px 0;
+    border-bottom: 1px solid var(--light-papirus);
+
+    @media screen and (max-width: 768px) {
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        width: auto;
+    }
+
+}
+
+.footer-box {
+    width: auto;
+    height: auto;
+    box-sizing: border-box;
+    overflow: hidden;
+
+    @media screen and (max-width: 768px) {
+        width: 100%;
+        margin: 16px 0;
+    }
+
+}
+
+.footer-box h3 {
+    color: var(--light-papirus);
+    font-family: var(--secondary-font);
+    font-size: 32px;
+    font-weight: 600;
+}
+
+.footer-box:nth-child(4) p {
+    text-decoration: underline;
+}
+
+.footer-box a {
+    color: var(--light-papirus);
+    font-family: var(--secondary-font);
+    font-size: 24px;
+    font-weight: 400;
+    text-decoration: none;
+
+}
+
+.copyright {
+    color: var(--light-papirus);
+    font-family: var(--secondary-font);
+    font-size: 20px;
+    font-weight: 400;
+    margin: 16px 0;
+
+}
 
 
 
