93dc90b8c314b08e182177daab131dcd61d394db
reasonsx <kamkre01@easv365.dk>
2024-05-24 18:33:44 +0200

    last update

--- 2nd-sem-project/src/assets/components/Home/HomeAbout.vue
+++ 2nd-sem-project/src/assets/components/Home/HomeAbout.vue
@@ -87,9 +87,14 @@ h2 {
 
     @media screen and (max-width: 768px) {
         width: 360px;
-        height: 180px;
+        height: 202px;
 
 
     }
+}
+iframe {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
 }
 </style>
